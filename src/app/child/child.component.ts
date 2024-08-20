80654ea56c866fb2768e97fb4439922cfd305b48
learndev77 <restrella@77global.biz>
2024-08-20 14:53:36 +0800

    complete commit

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,38 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  Input,
+  QueryList,
+  TemplateRef,
+} from '@angular/core';
+import { ChildContentComponent } from '../child-content/child-content.component';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
 })
-export class ChildComponent {
+export class ChildComponent implements AfterViewInit {
   @Input() child: TemplateRef<any> | null = null;
+  @ContentChild(ChildContentComponent) contentChild:
+    | ChildContentComponent
+    | undefined;
+
+  @ContentChildren(ChildContentComponent) contentChildren:
+    | QueryList<ChildContentComponent>
+    | undefined;
+
+  @ContentChildren('topThree') topThreeChildre:
+    | QueryList<ChildContentComponent>
+    | undefined;
 
   constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log('contentChild', this.contentChild);
+    console.log('contentChildren', this.contentChildren?.toArray());
+    console.log('contentChildren', this.topThreeChildre?.toArray());
+  }
 }
